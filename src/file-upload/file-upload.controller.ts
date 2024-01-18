2cd41597c6692c85e9717ef58d965a87b7a32ced
danielmachado <danielmachadopintos@gmail.com>
2024-01-18 14:29:06 -0300

    Add support for XLSX and CSV file processing

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -3,24 +3,58 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  BadRequestException,
 } from '@nestjs/common';
 import * as Multer from 'multer';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
+import * as XLSX from 'xlsx';
+import * as Papa from 'papaparse';
 
 @Controller('file-upload')
 export class FileUploadController {
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('files'))
   async uploadFile(@UploadedFile() file: Multer.File) {
     const fileExtName = extname(file.originalname).toLowerCase();
 
     if (fileExtName === '.xlsx') {
-      // Processamento para arquivo XLSX
+      this.processXLSXFile(file.buffer);
     } else if (fileExtName === '.csv') {
-      // Processamento para arquivo CSV
+      this.processCSVFile(file.buffer);
     } else {
-      throw new Error('Formato de arquivo não suportado');
+      throw new BadRequestException('Formato de arquivo não suportado.');
     }
+  }
+
+  processXLSXFile(file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+
+      const firstSheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[firstSheetName];
+
+      // Converte a planilha para JSON
+      const json = XLSX.utils.sheet_to_json(worksheet);
+
+      console.log('Dados da Planilha XLSX:', json);
+    };
+    reader.readAsBinaryString(file);
+  }
+
+  processCSVFile(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    Papa.parse(file, {
+      complete: (results) => {
+        console.log('Dados do CSV:', results.data);
+      },
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+    });
   }
 }
