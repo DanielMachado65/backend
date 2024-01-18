4bf0551b3a0aef3cecb9e3632bc03c2bc83e1f61
Daniel Machado <danielmachadopintos@gmail.com>
2024-01-18 09:59:41 -0300

    Add Swagger documentation and dependencies

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('metris')
 @Controller('metrics')
 export class MetricsController {
   constructor(private readonly metricsService: MetricsService) {}
